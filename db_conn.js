43b4183c01e9409a1a8f1a93cedc3daf239f87d4
Vortex7007 <125956254+Vortex7007@users.noreply.github.com>
2024-08-19 12:34:32 +0530

    connection stablished from db

---
+++ db_conn.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+const mongourl = "mongodb://127.0.0.1:27017/";
+
+mongoose.connect(mongourl)
+.then( ()=>{console.log("Mongo Db connection successful")})
+.catch((err) =>{console.log(err)});
