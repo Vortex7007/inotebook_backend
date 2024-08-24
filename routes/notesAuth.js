383ece76ad578b137858badb80ed9d137ba4a83f
Vortex7007 <125956254+Vortex7007@users.noreply.github.com>
2024-08-24 16:45:56 +0530

    add notes and fetching notes endpoints added

--- routes/notesAuth.js
+++ routes/notesAuth.js
@@ -1,8 +1,44 @@
 const express = require("express");
 const router = express.Router();
+const fetchuser = require('../middleware/fetchuser');
+const Note = require('../models/Note');
+const { body, validationResult } = require("express-validator");
 
-router.get('/',(req , res)=>{
-    res.json([])
+//ROUTE 1 : Get all the nOtes using : POST "/api/notes/fetchallnotes". Login required
+
+router.get('/fetchallnotes', fetchuser, async (req, res) => {
+    try {
+        const notes = await Note.find({ user: req.user.id });
+        res.json(notes)
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some Error Occured");
+    }
+})
+//ROUTE 2 : Add a new note using: POST "/api/notes/addnote". Login required
+
+router.post('/addnote', fetchuser, [
+    body('title', 'Enter a valid title').isLength({ min: 3 }),
+    body('description').isLength({ min: 5 })
+], async (req, res) => {
+    try {
+
+
+        const { title, description, tag } = req.body;
+        //If there are errors, return Bad request and the errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const note = new Note({
+            title, description, tag, user: req.user.id
+        })
+        const savedNote = await note.save();
+        res.json(savedNote)
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some Error Occured");
+    }
 })
 
 module.exports = router;
