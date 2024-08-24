83d8d69277c884d14ec55e58a4e6f3e05f20a51e
Vortex7007 <125956254+Vortex7007@users.noreply.github.com>
2024-08-24 21:49:51 +0530

    endpoint for updating notes added

--- routes/notesAuth.js
+++ routes/notesAuth.js
@@ -18,8 +18,8 @@ router.get('/fetchallnotes', fetchuser, async (req, res) => {
 //ROUTE 2 : Add a new note using: POST "/api/notes/addnote". Login required
 
 router.post('/addnote', fetchuser, [
-    body('title', 'Enter a valid title').isLength({ min: 3 }),
-    body('description').isLength({ min: 5 })
+    body('title', 'title must be atleast 3 characters').isLength({ min: 3 }),
+    body('description',"description must be atleast 5 characters").isLength({ min: 5 })
 ], async (req, res) => {
     try {
 
@@ -39,6 +39,30 @@ router.post('/addnote', fetchuser, [
         console.error(error.message);
         res.status(500).send("Some Error Occured");
     }
+})
+
+//ROUTE 3 :Update a note using: POST "/api/notes/updatenote". Login required
+
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+    const {title , description, tag} = req.body;
+    // Create a newNote object 
+    const newNote = {};
+    if(title){newNote.title = title};
+    if(description){newNote.description = description};
+    if(tag){newNote.tag = tag};
+    
+    //Find the note to be updated and update it 
+    let note =await Note.findById(req.params.id);
+    if(!note){
+        console.log("here")
+        return res.status(404).send("Not Found");
+    }
+    if(note.user.toString() !== req.user.id){
+        return res.status(404).send("Not Allowed");
+    }
+
+    note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote},{new:true})
+    res.json({note});
 })
 
 module.exports = router;
