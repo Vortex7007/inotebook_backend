78b8f5be0c1bbd1ebcb015460ee34b16726b0756
Vortex7007 <125956254+Vortex7007@users.noreply.github.com>
2024-08-29 16:34:15 +0530

    cors middleware added

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors');
 require("./db_conn.js");
 
 const app = express();
 const port = 5000
 
 //middlewares
+app.use(cors())
 app.use(express.json())
 
 //Available routes
